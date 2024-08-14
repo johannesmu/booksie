f7cb98e39d7681e7b5bfb6ec8a3696860e01c23e
johannes <johannes.muljana@ait.nsw.edu.au>
2024-08-14 16:00:25 +1000

    update to navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,39 @@ import Nav from "react-bootstrap/Nav"
 import Container from "react-bootstrap/Container"
 import Form from "react-bootstrap/Form"
 import InputGroup from "react-bootstrap/InputGroup"
+import Button from "react-bootstrap/Button"
+import Dropdown from "react-bootstrap/Dropdown"
 
 export function Header(props: any) {
     return (
-        <Navbar>
+        <Navbar expand="lg" className="bg-body-secondary justify-content-between">
             <Container fluid >
                 <Navbar.Brand>
                     <a href="#">{props.title}</a>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="main-nav" />
-                <Form className="flex-fill">
-                    <Form.Label className="visually-hidden">Search</Form.Label>
-                    <Form.Control type="search" />
+                <Form className="flex-grow-0 d-flex" role="search" style={{ maxWidth: "100vw", minWidth: "40vw" }}>
+                    <InputGroup>
+                        <Form.Label className="visually-hidden">Search</Form.Label>
+                        <Form.Control type="search" />
+                        <Button>Search</Button>
+                    </InputGroup>
                 </Form>
-                <Navbar.Collapse id="main-nav">
+                <Dropdown>
+                    <Dropdown.Toggle id="account-dd">
+                        Hey
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu variant="secondary">
+                        <Dropdown.Item>
+                            <Nav.Link href="/favourites">Favourites</Nav.Link>
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                            <Nav.Link href="/account">Account</Nav.Link>
+                            </Dropdown.Item>
+                        <Dropdown.Item><Nav.Link href="/basket">Basket</Nav.Link></Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Navbar.Collapse id="main-nav" className="flex-grow-0">
                     <Nav className="ms-auto">
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/books">Books</Nav.Link>
